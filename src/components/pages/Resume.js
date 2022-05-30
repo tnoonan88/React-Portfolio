335bcfc3e7af76550b0496fc38e14459ce3c4158
tnoonan <tnoonan88@gmail.com>
2022-05-30 13:10:12 -0400

    added downloadable resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,18 +4,27 @@ export default function Resume() {
   return (
     <div>
       <h1>Resume</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+
+      <h2><a href="../assets/images/myResume.pdf" download="Tom-Noonan-Resume">Download my resume here!</a></h2>
+
+      <h2>Front End:</h2>
+        <ul>
+          <li>HTML5</li>
+          <li>CSS3</li>
+          <li>Bootstrap</li>
+          <li>JavaScript</li>
+          <li>jQuery</li>
+          <li>Progressive Web Applications</li>
+        </ul>
+      <h2>Back End:</h2>
+        <ul>
+          <li>Node.js</li>
+          <li>MySQL</li>
+          <li>MongoDB</li>
+          <li>GraphQL</li>
+          <li>MERN Stack</li>
+          <li>React</li>
+        </ul>
     </div>
   );
 }
