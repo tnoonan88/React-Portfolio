6d5449f5f41449ba4390c15e5e763727e5e3e83b
tnoonan <tnoonan88@gmail.com>
2022-05-30 13:09:49 -0400

    added links and logo images to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,17 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="footer">
-      <h1>Goodbye</h1>
+      <a href='mailto:tnoonan88@gmail.com' rel='noreferrer' target='_blank'>
+        <img className='logo' src='../assets/images/gmail.png' alt='Gmail'/>
+      </a>
+
+      <a href='https://github.com/tnoonan88' rel='noreferrer' target='_blank'>
+        <img className='logo' src='../assets/images/github.png' alt='Github'/>
+      </a>
+
+      <a href='https://www.linkedin.com/in/tom-noonan-76aa5b190/' rel='noreferrer' target='_blank'>
+        <img className='logo' src='../assets/images/linkedin.png' alt='LinkedIn'/>
+      </a>
     </footer>
   );
 }
