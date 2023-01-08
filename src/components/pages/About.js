fde2f6ed6ffe4b42dd9cc3aa3301dbfccd2017d4
tnoonan <tnoonan88@gmail.com>
2023-01-08 18:40:13 -0500

    updated gh-pages

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -9,7 +9,7 @@ export default function About() {
       <div className='about-content'>
         <img className='pro' src={proShot} alt='headshot'/>
           <div className='aboutText'>
-            <p>I am a full-stack software developer and graduate of the Georgia Tech Full-Stack Web Developer bootcamp. I am proficient in both front end and server side development. I enjoy database managment and excel at MySQL, NoSQL and GraphQL development.</p>
+            <p>I am a full-stack software developer and graduate of the Georgia Tech Full-Stack Web Developer bootcamp. I am proficient in both front-end and server-side development. I enjoy database managment and excel at MySQL, NoSQL and GraphQL development.</p>
             <p>Presently, I work as a sales consultant in the fine wine and spirits industry. In my free time, I like to get outside! I am a runner, climber, cyclist and SCUBA diver.</p>
           </div>
       </div>
